91f0872edd2262502fc909d085c2deb5c7ba299e
Dave Mateer <davemateer@gmail.com>
2021-06-15 18:42:03 +0100

    auto

--- facesearch_cloud/1.sh
+++ facesearch_cloud/1.sh
@@ -1,3 +1,4 @@
 rm -rf cjob1/*.fe
-./faceservice_main.py -i ~/facesearch/job1/ -j 123
+# ./faceservice_main.py -i ~/facesearch/job1/ -j 123
+./faceservice_main.py -i cjob1/ -j 123
 # python3 sf-dave.py -i job1 -j 1
