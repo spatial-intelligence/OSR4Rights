2b3c603c0a898e8f3717cf11172fc88f7a20649c
Dave Mateer <davemateer@gmail.com>
2021-06-16 07:31:02 +0100

    auto

--- facesearch_cloud/1.sh
+++ facesearch_cloud/1.sh
@@ -1,6 +1,13 @@
 # delete the face encoding files - for load testing timings
+
+# 453s on CPU 
+# 7.5s on GPU
 # for load testing the GPU delete the face encoding files
-rm -rf cjob1/search/*.fe_cnn
-rm -rf cjob1/target/*.fe_cnn
+
+cd cjob1
+rm -f search/*.fe_cnn
+rm -f target/*.fe_cnn
+rm -rf results
+cd ..
 
 ./faceservice_main.py -i cjob1/ -j 123
