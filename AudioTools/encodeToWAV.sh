bc65a3709b29a0241adcc30fd06cfff4f0234c2c
Dave Mateer <davemateer@gmail.com>
2022-02-02 09:57:28 +0000

    fixed encode -y switch

--- AudioTools/encodeToWAV.sh
+++ AudioTools/encodeToWAV.sh
@@ -7,22 +7,22 @@ for filename in /home/dave/OSR4Rights/AudioTools/input/*.mp3; do
         # if no match on the above glob don't run ffmpeg 
         [ -e "$filename" ] || continue
 
-        ffmpeg -i -y "$filename" "${filename%.*}.wav"
+        ffmpeg -y -i "$filename" "${filename%.*}.wav"
 done
 
 for filename in /home/dave/OSR4Rights/AudioTools/input/*.ogg; do
         [ -e "$filename" ] || continue
-        ffmpeg -i -y "$filename" "${filename%.*}.wav"
+        ffmpeg -y -i "$filename" "${filename%.*}.wav"
 done
 
 for filename in /home/dave/OSR4Rights/AudioTools/input/*.flac; do
         [ -e "$filename" ] || continue
-        ffmpeg -i -y "$filename" "${filename%.*}.wav"
+        ffmpeg -y -i "$filename" "${filename%.*}.wav"
 done
 
 for filename in /home/dave/OSR4Rights/AudioTools/input/*.m4a; do
         [ -e "$filename" ] || continue
-        ffmpeg -i -y "$filename" "${filename%.*}.wav"
+        ffmpeg -y -i "$filename" "${filename%.*}.wav"
 done
 
 
@@ -31,11 +31,11 @@ done
 # sometimes youtube clips can be in this video format
 for filename in /home/dave/OSR4Rights/AudioTools/input/*.webm; do
         [ -e "$filename" ] || continue
-        ffmpeg -i -y "$filename" "${filename%.*}.wav"
+        ffmpeg -y -i "$filename" "${filename%.*}.wav"
 done
 
 
 for filename in /home/dave/OSR4Rights/AudioTools/input/*.mp4; do
         [ -e "$filename" ] || continue
-        ffmpeg -i -y "$filename" "${filename%.*}.wav"
+        ffmpeg -y -i "$filename" "${filename%.*}.wav"
 done
