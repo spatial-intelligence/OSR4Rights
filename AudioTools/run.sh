bb125809dedc16510fa85de7a95880c5cf82befa
Dave Mateer <davemateer@gmail.com>
2022-04-29 14:59:24 +0100

    auto

--- AudioTools/run.sh
+++ AudioTools/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python3 audiofile_reduce_to_speechparts.py -i /home/dave/OSR4Rights/AudioTools/input/ -j 123 >log.txt 
+python3 audiofile_reduce_to_speechparts.py -i /home/dave/OSR4Rights/AudioTools/input/ -j 123 >/home/dave/OSR4Rights/AudioTools/input/log.txt 
